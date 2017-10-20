4e23af80a6e4ecdf979c7476296aa495c037abf0
boB Rudis <bob@rud.is>
2017-10-20 15:26:52 -0400

    minor test tweak

--- tests/testthat/test-cdcfluview.R
+++ tests/testthat/test-cdcfluview.R
@@ -1,6 +1,8 @@
 context("basic functionality")
 test_that("we can do something", {
 
+  skip_on_cran()
+
   expect_that(dim(get_flu_data("hhs", years=2015)), equals(c(520L, 15L)))
 
   expect_that(dim(get_state_data(2008)), equals(c(2494L, 8L)))
